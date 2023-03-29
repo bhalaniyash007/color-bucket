67bc3d6e04d7d03705291a3571cbe77e302bd66e
Yash Bhalani <developer.yash.bhalani@gmail.com>
2023-03-29 12:09:49 -0300

    feat: display color code on mouse hover

--- app/components/ColorPalete/ColorCell.tsx
+++ app/components/ColorPalete/ColorCell.tsx
@@ -1,8 +1,41 @@
 import { SIZE } from "@theme/styles";
+import { THEME } from "@theme/theme";
 import { IColor } from "@types";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+
+interface IColorCode {
+  color: string;
+}
+const ColorCode = ({ color }: IColorCode) => {
+  return (
+    <>
+      <div
+        className="color-code"
+        style={{
+          padding: "5px",
+          borderRadius: "5px",
+          background: "#00000020",
+          marginTop: "25px",
+          marginLeft: "5px",
+          maxWidth: "80px",
+        }}
+      >
+        <p>{`#${color}`}</p>
+      </div>
+    </>
+  );
+};
 
 const ColorCell = ({ color }: IColor) => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(true);
+  };
+
+  const onLeave = () => {
+    setHover(false);
+  };
+
   return (
     <>
       <div
@@ -12,7 +45,11 @@ const ColorCell = ({ color }: IColor) => {
           width: SIZE.COLOR_CELL.WIDTH,
           height: SIZE.COLOR_CELL.HEIGHT,
         }}
-      ></div>
+        onMouseEnter={onHover}
+        onMouseLeave={onLeave}
+      >
+        {hover ? <ColorCode color={color} /> : <></>}
+      </div>
     </>
   );
 };
